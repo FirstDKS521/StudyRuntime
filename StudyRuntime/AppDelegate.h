365d002544a5b87f1d996cc9108072f3aa2bbc21
duks <duks@robooot.com>
2017-03-10 16:39:55 +0800

    Initial Commit

---
+++ StudyRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyRuntime
+//
+//  Created by aDu on 2017/3/10.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
